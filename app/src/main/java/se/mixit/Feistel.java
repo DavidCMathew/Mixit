96225e5a98b448ccebf35d3368fe715dff91b836
David <davidsemailid@gmail.com>
2017-04-15 17:32:42 +0530

    Added The feistel class -Contains methods encrypt, decrypt and f -finished encrypt and f method -Testing done on algorithm as a separate instance

---
+++ app/src/main/java/se/mixit/Feistel.java
@@ -0,0 +1,50 @@
+package se.mixit;
+
+/*
+        Class with the main methods for Encryption/Decryption
+ */
+public class Feistel {
+    public String encrypt(String PlainText,String Key){
+        if(PlainText.length()%2!=0)
+            PlainText+=' ';
+        char[] CipherTextArray=new char[PlainText.length()];
+        char Lo,Ln=0;
+        char Ro,Rn=0;
+        for(int j=0;j<PlainText.length();j+=2) {
+            Lo=PlainText.charAt(j);
+            Ro=PlainText.charAt(j+1);
+            for (int i = 0; i < Key.length(); i++) {
+
+                Ln = Ro;
+                Rn = (char) (Lo^ f(Ro,Key.charAt(i)));
+                Ro=Rn;
+                Lo=Ln;
+            }
+            CipherTextArray[j]=Rn;
+            CipherTextArray[j+1]=Ln;
+        }
+        String CipherText=new String(CipherTextArray);
+        return CipherText;
+    }
+
+
+
+    public String decrypt(String CipherText,String Key){
+        String PlainText=CipherText;
+
+        return PlainText;
+    }
+
+
+
+    //f function for the feistel network returns a modified P
+    private static char f(char R,char K) {
+        char P = 0;
+        try {
+            P = (char) ((short) ((R % K)) >> 2);
+        }catch (ArithmeticException e){P=R;}
+        finally {
+            return P;
+        }
+    }
+}
